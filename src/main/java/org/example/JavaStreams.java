7ce96ebe24691be7390ff44a0abf990759f9e701
Nicholo Patrick Pardines <nick123abl@gmail.com>
2025-07-07 14:12:57 +0800

    Added activity 5

--- src/main/java/org/example/JavaStreams.java
+++ src/main/java/org/example/JavaStreams.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -25,12 +26,29 @@ public class JavaStreams {
         List<Integer> squaredArray = squareArray.stream().map(num -> num*num).toList();
         System.out.println(squaredArray);
 
-//        Activity 4:
+//        Activity 4: Even and Greater Than Five
         List<Integer> toSortArray = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         Predicate<Integer> isEven = s->s%2==0;
         Predicate<Integer> greaterThanFive = s -> s>5;
-        List<Integer> filteredArray = toSortArray.stream().filter(num -> isEven.test(num)
-                && greaterThanFive.test(num)).toList();
+        Predicate<Integer> isEvenAndGreaterThanFive = isEven.and(greaterThanFive);
+        List<Integer> filteredArray = toSortArray.stream().filter(isEvenAndGreaterThanFive).toList();
         System.out.println(filteredArray);
+
+        //    Activity 5: Sort Objects Using Comparator
+        List<Person> personList = Arrays.asList(
+                new Person("Nick",800),
+                new Person("James", 18),
+                new Person("Shaquille",35),
+                new Person("Kanye",60),
+                new Person("Michael Jordan",45)
+        );
+//    Sort by Age (Ascending Order)
+//    Sort by Name (Alphabetical Order)
+//    Sort by Age (Descending Order)
+        Comparator<Person> ascendingAge = (p1,p2) -> p1.getAge() - p2.getAge();
+        Comparator<Person> descendingAge = (p1,p2)-> p2.getAge() - p1.getAge();
+        System.out.println("Sorted by age (ascending): "+personList.stream().sorted(ascendingAge).toList());
+        System.out.println("Sorted by name (alphabetical):"+personList.stream().sorted(Comparator.comparing(Person::getName)).toList());
+        System.out.println("Sorted by age (descending): "+personList.stream().sorted(descendingAge).toList());
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Person {
+    String name;
+    int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    int getAge(){
+        return this.age;
+    }
+    String getName(){
+        return this.name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}'+"\n";
+    }
+}
